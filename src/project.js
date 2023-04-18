c690178b3c71b92964321d433e3170230e717c11
chinenye whitney Ekugo <nenyeekugo@gmail.com>
2023-04-18 16:47:38 +0100

    Added icon url

--- src/project.js
+++ src/project.js
@@ -6,8 +6,9 @@ function formatDate(timestamp) {
   }
   let minutes = date.getMinutes();
   if (minutes < 10) {
-    hours = `0${minutes}`;
+    minutes = `0${minutes}`;
   }
+
   let days = [
     "Sunday",
     "Monday",
@@ -17,25 +18,55 @@ function formatDate(timestamp) {
     "Friday",
     "Saturday",
   ];
-  let day = days[date.getDate()];
+  let day = days[date.getDay()];
+
+  let months = [
+    "January",
+    "February",
+    "March",
+    "April",
+    "May",
+    "June",
+    "July",
+    "August",
+    "September",
+    "October",
+    "November",
+    "December",
+  ];
+
+  let month = months[date.getMonth()];
+  let dates = date.getDate();
+  let timeElement = document.querySelector("#time");
+  timeElement.innerHTML = `${hours}:${minutes}`;
+
+  return `${day}, ${dates} ${month}`;
 }
 
 function displayWeather(response) {
   console.log(response.data);
-  console.log(response.data.time);
+  console.log(response.data.time * 1000);
   let weatherElement = document.querySelector("#weather");
   let humidityElement = document.querySelector("#Humidity");
   let windElement = document.querySelector("#wind");
   let descriptionElement = document.querySelector("#description");
   let dateElement = document.querySelector("#date");
+  let iconElement = document.querySelector("#icon");
+  let countryElement = document.querySelector("#country");
   weatherElement.innerHTML = Math.round(response.data.temperature.current);
   humidityElement.innerHTML = response.data.temperature.humidity;
   windElement.innerHTML = Math.round(response.data.wind.speed);
   descriptionElement.innerHTML = response.data.condition.description;
+  countryElement.innerHTML = `${city}, ${response.data.country}`;
   dateElement.innerHTML = formatDate(response.data.time * 1000);
+  iconElement.setAttribute(
+    "src",
+    `http://shecodes-assets.s3.amazonaws.com/api/weather/icons/${response.data.condition.icon}.png`
+  );
 }
 
+let city = "Toronto";
 let apiKey = `f8o9e9ae7af783f9513a465db0bta63f`;
-let apiUrl = `https://api.shecodes.io/weather/v1/current?query=Abuja&key=${apiKey}&units=metric`;
+let apiUrl = `https://api.shecodes.io/weather/v1/current?query=${city}&key=${apiKey}&units=metric`;
 
 axios.get(apiUrl).then(displayWeather);
