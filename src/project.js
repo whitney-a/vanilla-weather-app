3a360d614ba5e504cfe125b65b74dafd4edb814c
chinenye whitney Ekugo <nenyeekugo@gmail.com>
2023-04-18 14:33:03 +0100

    Restyled some CSS elements

--- src/project.js
+++ src/project.js
@@ -1,13 +1,38 @@
+function formatDate(timestamp) {
+  let date = new Date(timestamp);
+  let hours = date.getHours();
+  if (hours < 10) {
+    hours = `0${hours}`;
+  }
+  let minutes = date.getMinutes();
+  if (minutes < 10) {
+    hours = `0${minutes}`;
+  }
+  let days = [
+    "Sunday",
+    "Monday",
+    "Tuesday",
+    "Wednesday",
+    "Thursday",
+    "Friday",
+    "Saturday",
+  ];
+  let day = days[date.getDate()];
+}
+
 function displayWeather(response) {
   console.log(response.data);
+  console.log(response.data.time);
   let weatherElement = document.querySelector("#weather");
   let humidityElement = document.querySelector("#Humidity");
   let windElement = document.querySelector("#wind");
   let descriptionElement = document.querySelector("#description");
+  let dateElement = document.querySelector("#date");
   weatherElement.innerHTML = Math.round(response.data.temperature.current);
   humidityElement.innerHTML = response.data.temperature.humidity;
   windElement.innerHTML = Math.round(response.data.wind.speed);
   descriptionElement.innerHTML = response.data.condition.description;
+  dateElement.innerHTML = formatDate(response.data.time * 1000);
 }
 
 let apiKey = `f8o9e9ae7af783f9513a465db0bta63f`;
